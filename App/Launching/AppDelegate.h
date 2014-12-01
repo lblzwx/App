b67654e21762614da9da6178896c0c4f1e815307
lblzwx <382434266@qq.com>
2014-12-01 21:47:18 +0800

    工程创建

---
+++ App/Launching/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  App
+//
+//  Created by 李保路 on 14-12-1.
+//  Copyright (c) 2014年 IT-Hamal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
